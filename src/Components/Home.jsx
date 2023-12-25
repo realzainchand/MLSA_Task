51c12de6b993d97300bf067cb7da2e54865062f9
realzainchand <zainchand222@gmail.com>
2023-12-25 22:44:30 +0500

    updated files

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/background1.jpg";
+import image from "../images/background2.jpg";
 
 const imageAltText = "A cyberpunk background photo";
 
